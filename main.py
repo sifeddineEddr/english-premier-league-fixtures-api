740ea5c498de3c31b925863eab1e8251fe184338
sifeddineeddr <sifeddine.edr@gmail.com>
2023-08-01 13:48:57 +0100

    get team fixtures function

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import pandas as pd
 
 data = pd.read_csv("FPL_Schedule2324.csv")
-print(data)
+
+# # Getting each row data
+# for index, row in data.iterrows():
+#     print(row)
+
+# Filtering
+# print(data.loc[data['Team'] == 'Arsenal'])
+
+
+def getTeamFixtures(team):
+    team_fixtures = data.loc[data["Team"] == team.capitalize()]
+    gameweeks_dict = {
+        col: team_fixtures[col].iloc[0]  # Create a dictionary with gameweek as key and fixture as value
+        for col in team_fixtures.columns if col != "Team"  # Exclude the 'Team' column
+    }
+    team_dict = {
+        "Team": team_fixtures["Team"].iloc[0],  # Get the team name from the first row
+        "Gameweeks": gameweeks_dict
+    }
+    return team_dict
+
+
+print(getTeamFixtures("arsenal"))
