9aed3608cf989c686dcc64ae17ec28c5e51f3845
sifeddineeddr <sifeddine.edr@gmail.com>
2023-08-01 14:53:24 +0100

    get all gameweeks function

--- main.py
+++ main.py
@@ -18,9 +18,35 @@ def getTeamFixtures(team):
     }
     team_dict = {
         "Team": team_fixtures["Team"].iloc[0],  # Get the team name from the first row
-        "Gameweeks": gameweeks_dict
+        "Gameweeks": gameweeks_dict,
     }
     return team_dict
 
 
-print(getTeamFixtures("arsenal"))
+# print(getTeamFixtures("arsenal"))
+
+def getAllGameweeks():
+    all_gameweeks = {}
+    already_printed = set()
+
+    for i in range(1, 39):
+        gameweek_column = f"GW{i}"
+        gameweeks_data = zip(data["Team"], data[gameweek_column])
+        fixtures = []
+
+        # To check if the fixture is already on the dictionnary
+        for team, fixture in gameweeks_data:
+            fixture_pair_1 = f"{team} vs {fixture}"
+            fixture_pair_2 = f"{fixture} vs {team}"
+            if (
+                fixture_pair_1 not in already_printed
+                and fixture_pair_2 not in already_printed
+            ):
+                fixtures.append(fixture_pair_1)
+
+        all_gameweeks[gameweek_column] = fixtures
+
+    return all_gameweeks
+
+
+print(getAllGameweeks())
